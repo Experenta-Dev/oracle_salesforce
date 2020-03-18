21504bed0eb67c9cfd43c8099f18dd91bb12b48a
Eduardo Vásquez <30580824+EduardoVasquez9@users.noreply.github.com>
2020-03-17 23:38:27 -0600

    Integración API-Heroku exitosa

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const pg = require('pg');
+const { Client } = require('pg');
 const bodyParser = require("body-parser");
 require("dotenv/config");
 const app = express();
@@ -10,6 +10,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar conexión, middlewares y rutas
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT name, pais_de_trabajo__c, email FROM salesforce.Contact', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log({Contact: row});
+    }
+    client.end();
+  });
 
 
 // inicializar
