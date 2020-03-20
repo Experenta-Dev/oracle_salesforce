920e4ecd004e1bf7c80403b1073a33b24002fb6a
Eduardo Vásquez <30580824+EduardoVasquez9@users.noreply.github.com>
2020-03-19 20:28:29 -0600

    Clients - completar el INSERT

--- app.js
+++ app.js
@@ -15,15 +15,36 @@ const client = new Client({
   ssl: true,
 });
 
-client.connect();
+client.connect( () =>{
+  console.log('Connected');
+});
 
-client.query('SELECT name, pais_de_trabajo__c, email FROM salesforce.Contact', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log({Contact: row});
-    }
-    client.end();
+// Clientes
+app.get('/clients', (req, res)=>{
+  client.query('SELECT * FROM salesforce.Contact', (err, data)=>{
+    res.json(data.rows);
   });
+});
+
+app.get('/clients/:id', (req, res)=>{
+  client.query('SELECT * FROM salesforce.Contact WHERE id = $1', [req.params.id], (err, data)=>{
+    res.json(data.rows[0]);
+  });
+});
+
+app.post('/clients', (req, res) => {
+  client.query('INSERT INTO salesforce.Contact(numeroidentificacion__c) VALUES ($1)', [req.body.numeroidentificacion__c], (err, data) => {
+    res.json(data);
+  });
+});
+
+app.delete('/clients/:sfid', function(req, res) {
+  client.query('DELETE FROM salesforce.Contact WHERE sfid = $1', [req.params.sfid], function(error, data) {
+    res.json(data);
+  });
+});
+
+// Facturas
 
 
 // inicializar
