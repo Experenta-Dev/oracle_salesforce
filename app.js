990a6374cddbb6d2e0d555963d42faadf2c40abf
Alexy Cruz <alexyc@unitec.edu>
2020-04-01 12:49:02 -0600

    json changed

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post('/clients', (req, response) => {
 
     (err, data) => {
       if (err) {
-        response.send(data);
+        response.status(400).send(err);
       } else {
         response.send(data.rows[0]);
       }
