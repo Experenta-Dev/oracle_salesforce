14a84e41589f804d17f0c126a50e39824d34f6f5
Alexy Cruz <alexyc@unitec.edu>
2020-04-19 04:14:43 -0600

    changes in client

--- app.js
+++ app.js
@@ -48,25 +48,26 @@ app.post('/clients', (req, response) => {
   let createdDate = new Date();
 
   client.query("INSERT INTO " +  
-    "salesforce.contact(Apellidos__c, LastName, Birthdate, CargoEmpresa__c, Ciudad_Tutor__c, Ciudad_de_Trabajo__c, Codigo_Cliente__c, CreatedDate," +
-      "Currency_Oracle__c, Custom_Form__c, Department__c, Direcci_n_Trabajo__c, Direccion_de_Casa__c, Distrito__c, Email, Empresa__c," +
-      "Estado_Civil__c, Estado__c, Fax_Oracle__c, Fecha_Caducidad_Tarjeta__c, HomePhone, Id, Individual__c, IsDeleted, Is_Company__c," +
-      "Is_Person__c, LastName, MobilePhone, Name, Nombre_Empresa__c, Nombre_Legal__c, Nombre_Madre__c, Nombre_Padre__c, Nombre_Titular_Tarjeta__c, Nombres__c," +
-      "NumeroIdentificacion__c, Numero_Tarjeta__c, Ocupacion__c, OtherPhone, Otra_Direccion__c, Pais_de_Trabajo__c, Pais_de_residencia__c, Parent_Company__c, Phone," + 
-      "Subsidiaria__c, SystemModstamp, TipoIdentificacion__c, Tipo_Tarjeta__c, hed__Gender__c, Contact_Ext_Id__c, Categoria_Oracle__c, Pais_Cliente_Oracle__c)" +
+    "salesforce.contact(Name, Custom_Form__c, Customer_ID__c, Status_Text__c, Type__c, Empresa__c, Nombre_Empresa__c, Ocupacion__c, " + 
+      "Pais_de_Trabajo__c, Ciudad_de_Trabajo__c, Direcci_n_Trabajo__c, Individual__c, Nombres__c, Apellidos__c, Nombre_Empresa_2__c, Nombre_Legal__c, Parent_Company__c, " +
+      "Categoria_Oracle__c, TipoIdentificacion__c, NumeroIdentificacion__c, Birthdate, Genero_Text__c, Estado_Civil__c, Nombre_Padre__c, Nombre_Madre__c, " +
+      "Email, Telefono_Oracle__c, Phone, MobilePhone, OtherPhone, Fax_Oracle__c, Subsidiaria__c, Fecha_Caducidad_Tarjeta__c, Nombre_Titular_Tarjeta__c, Pais_Cliente_Oracle__c, " +
+      "Tipo_Tarjeta__c, Numero_Tarjeta__c, Currency_Oracle__c, Pais_Cliente_Oracle_2__c, Department__c, Ciudad_Tutor__c, Distrito__c, " +
+      "Direccion_de_Casa__c, Otra_Direccion__c, Internal_ID_Oracle, CreatedDate, Id, IsDeleted, SystemModstamp, LastName)" +
 
     "VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34," +
-      "$35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50, $51, $52) RETURNING *", 
+      "$35, $36, $37, $38, $39, $40, $41, $42, $43, $44, $45, $46, $47, $48, $49, $50) RETURNING *", 
     
-    [req.body.apellidos, req.body.LastName, req.body.fecha_nacimiento, req.body.ocupacion, req.body.ciudad, req.body.ciudad_empresa, req.body.codigo_cliente, createdDate, 
-    req.body.currency, req.body.customform, req.body.departamento, req.body.direccion_empresa, req.body.addr1, req.body.distrito, 
-    req.body.email, req.body.empresa_trabaja, req.body.estado_civil, req.body.entitystatus, req.body.fax, 
-    req.body.fecha_caducidad_tarjeta, req.body.homephone, req.body.internal_id_saleforce, req.body.individual, req.body.IsDeleted,
-    req.body.is_company, req.body.is_person, req.body.apellidos, req.body.mobilephone, req.body.Name, req.body.companyname, 
-    req.body.legal_name, req.body.nombre_madre, req.body.nombre_padre, req.body.nombre_titular_tarjeta, 
-    req.body.nombres, req.body.numero_identificacion, req.body.numero_tarjeta_credito, req.body.ocupacion,
-    req.body.altphone, req.body.addr2, req.body.pais_de_empresa, req.body.country, req.body.parent, req.body.phone, req.body.subsidiary,
-    createdDate, req.body.tipo_identifiacion, req.body.tipo_tarjeta, req.body.genero, req.body.Contact_Ext_Id__c, req.body.category, req.body.pais_cliente], 
+    [req.body.custentity_internal_id_saleforce, req.body.customform, req.body.entityid, req.body.entitystatus, req.body.isperson, req.body.company,
+      req.body.custentity_empresa_trabaj, req.body.custentity_ocupacio, req.body.custentity_pais_de_empres, req.body.custentity_ciudad_empres, 
+      req.body.custentity_direccion_empres, req.body.individua, req.body.firstnam, req.body.lastname,
+      req.body.companyname, req.body.custentity_legal_name, req.body.parent, req.body.category, req.body.custentity_tipo_identifiacion, 
+      req.body.custentity_numero_identificacion, req.body.custentity_fecha_nacimiento, req.body.custentity_genero, req.body.custentity_estado_civil,
+      req.body.custentity_nombre_padre, req.body.custentity_nombre_madre, req.body.email, req.body.phone, req.body.homephone, req.body.mobilephone, req.body.altphone,
+      req.body.fax, req.body.subsidiary, req.body.custentity_fecha_caducidad_tarjeta, req.body.custentity_nombre_titular_tarjeta,
+      req.body.custentity_pais_cliente, req.body.custentity_tipo_tarjeta, req.body.custentity_numero_tarjeta_credito, req.body.currency,
+      req.body.country, req.body.custrecord_departamento, req.body.custrecord_ciudad, req.body.custrecord_distrito, req.body.addr1,
+      req.body.addr2, req.body.internalid, req.body.CreatedDate, req.body.Id, req.body.IsDeleted, req.body.SystemModstamp, req.body.LastName], 
 
     (err, data) => {
       if (err) {
